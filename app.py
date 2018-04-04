517284a7137ca0c3ca637e4fa6a22f9bd9180d95
Andrew Thomas <andrew@cjthomas.org>
2018-04-03 21:33:52 -0400

    trying to add db

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import datetime
 import os.path
 import time
+import psycopg2
 from flask import Flask, render_template, redirect, url_for
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
@@ -13,6 +14,8 @@ app = Flask(__name__)
 
 # set the secret key.
 app.secret_key = 'developer'
+
+DATABASE_URL = os.environ['DATABASE_URL']
 
 
 class ProgramForm(FlaskForm):
@@ -29,15 +32,20 @@ def submit():
     if form.validate_on_submit():
         f = form.program.DataRequired
         filename = secure_filename(f.filename)
-        f.save(os.path.join(
-            app.instance_path,
-            'programs',
-            '{0}-{1}-{2}'.format(
-                datetime.datetime.fromtimestamp(
-                    time.time()).strftime('%Y-%m-%d_%H:%M:%S'),
-                form.email.data,
-                filename)
-        ))
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        cur = conn.cursor()
+        cur.execute('INSERT INTO programs (email, filename, file_bin) VALUES \
+                        (%s, %s, %s)',
+                    (form.email.data, filename, form.program.data))
+        # f.save(os.path.join(
+        #     app.instance_path,
+        #     'programs',
+        #     '{0}-{1}-{2}'.format(
+        #         datetime.datetime.fromtimestamp(
+        #             time.time()).strftime('%Y-%m-%d_%H:%M:%S'),
+        #         form.email.data,
+        #         filename)
+        # ))
         return redirect(url_for('submit'))
     else:
         print(form.errors)
