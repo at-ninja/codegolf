157a94e852577b7b5beef4f9912ae55f15d38512
Andrew Thomas <andrew@cjthomas.org>
2018-04-08 21:58:12 -0400

    Adding a bunch of things, but mainly moving away from heroku for the time being, until i can find a way to store what i want to without heroku's limitations

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import datetime
 import os.path
 import time
-import urllib.parse as urlparse
-import psycopg2
+# import codejail
+# import urllib.parse as urlparse
+# import psycopg2
 from flask import Flask, render_template, redirect, url_for
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
@@ -16,24 +17,16 @@ app = Flask(__name__)
 # set the secret key.
 app.secret_key = 'developer'
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
-def open_db_conn():
-    urlparse.uses_netloc.append("postgres")
-    url = urlparse.urlparse(DATABASE_URL)
-    conn = psycopg2.connect(
-        database=url.path[1:],
-        user=url.username,
-        password=url.password,
-        host=url.hostname,
-        port=url.port)
-    return conn
-
+descriptions = {
+    '1': 'This is an example question. I\'m trying to see if inline <b>HTML</b> works'
+}
 
 class ProgramForm(FlaskForm):
     program = FileField(validators=[FileRequired()])
     email = EmailField(
         validators=[validators.DataRequired(), validators.Email()])
+    problem = SelectField(choices=[(str(x), 'Problem {}'.format(x+1)) for x in range(9)],
+                           validators=[validators.DataRequired()])
     language = SelectField(choices=[('0', 'Python3'), ('1', 'Python2'), ('2', 'Node'), ('3', 'C/C++'), ('4', 'Java')],
                            validators=[validators.DataRequired()])
 
@@ -41,20 +34,35 @@ class ProgramForm(FlaskForm):
 @app.route('/', methods=('GET', 'POST'))
 def submit():
     form = ProgramForm()
+    if form.problem is not None:
+        print(form.problem.data)
     if form.validate_on_submit():
+        timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
         f = form.program.data
         filename = secure_filename(f.filename)
-        conn = open_db_conn()
-        cur = conn.cursor()
-        cur.execute('INSERT INTO programs (email, problem, filename, language, file_blob) VALUES \
-                        (%s, \'test\', %s, \'test\', %s);',
-                    (form.email.data, filename, f.read()))
-        conn.commit()
-        conn.close()
+        f.save(os.path.join(
+            app.instance_path, 'tmp', '{0}-{1}-{2}'.format(timestamp, form.email.data, filename)
+        ))
+        size = os.path.getsize(os.path.join(
+            app.instance_path, 'tmp', '{0}-{1}-{2}'.format(timestamp, form.email.data, filename)
+        ))
+        os.remove(os.path.join(
+            app.instance_path, 'tmp', '{0}-{1}-{2}'.format(timestamp, form.email.data, filename)
+        ))
+        f.save(os.path.join(
+            app.instance_path, 'programs', '{4}-{0}-{1}-{2}-{3}'.format(size, timestamp, form.email.data, filename, form.problem.data)
+        ))
         return redirect(url_for('submit'))
     else:
         print(form.errors)
     return render_template('index.html', form=form)
+
+@app.route('/problem/<problemnumber>')
+def problem(problemnumber):
+    if problemnumber in ['1','2','3','4','5','6','7','8','9']:
+        return render_template('problem.html', problem=problemnumber)
+    else:
+        return redirect(url_for('submit'))
 
 
 if __name__ == '__main__':
